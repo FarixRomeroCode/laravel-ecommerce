3798d827ee400e1d3b40580efbeab160269f93f7
Farix Romero <farixromero123@gmail.com>
2020-08-04 23:39:39 -0500

    practice

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -18,15 +18,35 @@
 
     <div class="cart-section container">
         <div>
-            <h2>3 items in Shopping Cart</h2>
+            @if (session()->has('sucess_message'))
+                <div class="alert alert-sucesss">
+                    {{session()->get('sucess_message')}}
+                </div>
 
+            @endif
+
+            @if(count($errors)>0){
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($error->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+
+            }
+            @endif
+
+            @if (Cart::count()>0)
+            <h2>{{Cart::count()}} items in Shopping Cart </h2>
+            @foreach(Cart::content() as $item)
             <div class="cart-table">
                 <div class="cart-table-row">
                     <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
+                    <a href="{{ route('shop.show',$item->model->slug)}}"><img src="{{asset('img/products/'.$item->model->slug.'.jpg')}}" alt="item" class="cart-table-img"></a>
                         <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
+                        <div class="cart-table-item"><a href="{{route('shop.show',$item->model->slug)}}">{{$item->model->name}}</a></div>
+                            <div class="cart-table-description">{{$item->model->details}}</div>
                         </div>
                     </div>
                     <div class="cart-table-row-right">
@@ -43,61 +63,10 @@
                                 <option>5</option>
                             </select>
                         </div>
-                        <div>$2499.99</div>
+                    <div>{{$item->model->presentPrice()}}</div>
                     </div>
                 </div> <!-- end cart-table-row -->
-
-                <div class="cart-table-row">
-                    <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                        <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                        </div>
-                    </div>
-                    <div class="cart-table-row-right">
-                        <div class="cart-table-actions">
-                            <a href="#">Remove</a> <br>
-                            <a href="#">Save for Later</a>
-                        </div>
-                        <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                        <div>$2499.99</div>
-                    </div>
-                </div> <!-- end cart-table-row -->
-
-                <div class="cart-table-row">
-                    <div class="cart-table-row-left">
-                        <a href="#"><img src="/img/macbook-pro.png" alt="item" class="cart-table-img"></a>
-                        <div class="cart-item-details">
-                            <div class="cart-table-item"><a href="#">MacBook Pro</a></div>
-                            <div class="cart-table-description">15 inch, 1TB SSD, 32GB RAM</div>
-                        </div>
-                    </div>
-                    <div class="cart-table-row-right">
-                        <div class="cart-table-actions">
-                            <a href="#">Remove</a> <br>
-                            <a href="#">Save for Later</a>
-                        </div>
-                        <div>
-                            <select class="quantity">
-                                <option selected="">1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                        <div>$2499.99</div>
-                    </div>
-                </div> <!-- end cart-table-row -->
+                @endforeach
 
             </div> <!-- end cart-table -->
 
@@ -133,6 +102,11 @@
                 <a href="#" class="button">Continue Shopping</a>
                 <a href="#" class="button-primary">Proceed to Checkout</a>
             </div>
+            @else 
+
+            <h3>No items in the Cart!!</h3>
+
+            @endif
 
             <h2>2 items Saved For Later</h2>
 
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ Route::get('/', 'LandingPageController@index')->name('landing-page');
 
 Route::get('/shop', 'ShopController@index')->name('shop.index');
 Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
+Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/cart', 'CartController@store')->name('cart.store');
+Route::get('empty',function(){
 
-Route::view('/cart', 'cart');
+    Cart::destroy();
+
+});
 Route::view('/checkout', 'checkout');
 Route::view('/thankyou', 'thankyou');
